9eab062e3d8c30ada093a7de55e2bb3838b61ad9
reddevill007 <shivampandey2900@gmail.com>
2023-03-06 20:03:01 +0530

    ui changes

--- pages/dashboard/[mood].jsx
+++ pages/dashboard/[mood].jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 export default function Dashboard() {
     const [mood, setMood] = useState("");
     const [playlist, setPlaylist] = useState([]);
-    const [tracks, setTracks] = useState([])
+    const [track, setTrack] = useState(1)
     const router = useRouter();
 
     const fetchData = () => {
@@ -16,12 +16,10 @@ export default function Dashboard() {
             }
         };
 
-        fetch('https://deezerdevs-deezer.p.rapidapi.com/playlist/6090542704', options)
+        fetch('https://deezerdevs-deezer.p.rapidapi.com/playlist/7618573602', options)
             .then(response => response.json())
             .then(response => setPlaylist(response))
             .catch(err => console.error(err));
-
-        // setTracks(playlist.tracks.data)
     }
 
     useEffect(() => {
@@ -37,16 +35,31 @@ export default function Dashboard() {
 
     return (
         <div className="bg-black min-h-screen text-white">
-            <img src={playlist.picture_big} alt="" />
-            {playlist?.tracks?.data?.slice(0, 10).map(play => {
-                return (
-                    <div key={play.id}>
-                        <img className="h-10 w-10 rounded-full" src={play.album.cover_big} alt="" />
-                        <p>{play.title}</p>
-                        <audio src={play.preview} controls />
-                    </div>
-                )
-            })}
+            <img src={playlist.picture_big} alt="" className="mb-10" />
+            <div className="space-y-3 pb-40">
+                {playlist?.tracks?.data.slice(0, 16).map((play, i) => {
+                    return (
+                        <div key={play.id} onClick={() => setTrack(i)} className='flex gap-10 border-b border-gray-900 cursor-pointer hover:bg-gray-900 rounded-lg items-center space-y-2 pb-4'>
+                            <img className="h-10 w-10 rounded-full" src={play.album.cover_big} alt="" />
+                            <p>{play.title}</p>
+                        </div>
+                    )
+                })}
+            </div>
+
+            <div className="fixed bottom-0 right-0 w-full bg-red-500">
+                {playlist?.tracks?.data.map((play, i) => {
+                    if (i === track) {
+                        return (
+                            <div key={play.id} className="w-full flex justify-between items-center px-10">
+                                <img className="h-10 w-10 rounded-full" src={play.album.cover_big} alt="" />
+                                <audio src={play.preview} controls className="bg-transparent" />
+                                <p>{play.title}</p>
+                            </div>
+                        )
+                    }
+                })}
+            </div>
         </div>
     )
 }
