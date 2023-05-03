6e01a50a83f352484d9bebf45692697f34c2e32c
reddevill007 <shivampandey2900@gmail.com>
2023-05-03 21:15:12 +0530

    SEO optimization

--- components/Navigation/Navbar.jsx
+++ components/Navigation/Navbar.jsx
@@ -1,9 +1,21 @@
 import { signOut } from "next-auth/react"
+import Image from "next/image"
+import Link from "next/link"
 
 const Navbar = () => {
     return (
         <div className="flex w-full justify-between items-center fixed top-0 left-0 px-3">
-            <div>Logo</div>
+            <div>
+                <Link href="/">
+                    <Image
+                        src="/images/logo.png"
+                        alt="moodify logo"
+                        height={20}
+                        width={20}
+                        className="w-10 h-10"
+                    />
+                </Link>
+            </div>
             <button onClick={() => signOut()}>Signout</button>
         </div>
     )
--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,30 @@ import { RecoilRoot } from "recoil";
 import { SessionProvider } from "next-auth/react";
 
 import Navbar from "../components/Navigation/Navbar";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <>
       <SessionProvider session={session}>
         <RecoilRoot>
+          <Head>
+            <title>Moodify - Recognition</title>
+            <meta
+              name="description"
+              content="Moodify is a web application that uses facial recognition to detect the user's mood and play a song that matches the mood."
+            />
+            <meta
+              name="keywords"
+              content="Moodify, Mood, Recognition, Facial, Face, Emotion, Emotions, Music, Spotify, Song, Songs, Playlist, Playlists, Moodify, Mood, Recognition, Facial, Face, Emotion, Emotions, Music, Spotify, Song, Songs, Playlist, Playlists, Moodify, Mood, Recognition, Facial, Face, Emotion, Emotions, Music, Spotify, Song, Songs, Playlist, Playlists, Moodify, Mood, Recognition, Facial, Face, Emotion, Emotions, Music, Spotify, Song, Songs, Playlist, Playlists, Moodify, Mood, Recognition, Facial, Face, Emotion, Emotions, Music, Spotify, Song, Songs, Playlist, Playlists, Moodify, Mood, Recognition, Facial, Face, Emotion, Emotions, Music, Spotify, Song, Songs, Playlist, Playlists"
+            />
+            <meta name="author" content="Moodify" />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1.0"
+            />
+            <link rel="icon" href="/images/logo.png" />
+          </Head>
           <Navbar />
           <Component {...pageProps} />
         </RecoilRoot>
