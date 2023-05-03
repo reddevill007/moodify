afef37c166f4aa4628303c1e30db31aba44a7b36
reddevill007 <shivampandey2900@gmail.com>
2023-05-03 08:47:48 +0530

    bugs fixes

--- pages/dashboard/[mood].jsx
+++ pages/dashboard/[mood].jsx
@@ -98,83 +98,83 @@ export default function Dashboard() {
         }
     }, [router.isReady]);
 
-
     return (
         <div className="relative container mx-auto flex items-center justify-center">
             {/* Head */}
             <Head>
                 <title>Moodify - Result:{mood}</title>
             </Head>
+            <>
+                {/* Center */}
+                <div className="text-blue-800 w-full p-4">
+                    {isLoading && <h1 className="text-white">Loading...</h1>}
 
-            {/* Center */}
-            <div className="text-blue-800 w-full p-4">
-                {isLoading && <h1 className="text-white">Loading...</h1>}
+                    {/* Banner */}
+                    <div className="w-full h-[300px] mx-auto mb-10 rounded-xl relative">
+                        <img src={playlist.picture_big} alt="" className="mb-10 h-full w-full object-cover absolute top-0 left-0 rounded-xl" />
+                        <div className="absolute z-20 top-0 left-0 w-full h-full rounded-xl bg-black bg-opacity-60">
+                            <h3 className="text-3xl text-white p-2">{playlist.title}</h3>
+                        </div>
+                    </div>
 
-                {/* Banner */}
-                <div className="w-full h-[300px] mx-auto mb-10 rounded-xl relative">
-                    <img src={playlist.picture_big} alt="" className="mb-10 h-full w-full object-cover absolute top-0 left-0 rounded-xl" />
-                    <div className="absolute z-20 top-0 left-0 w-full h-full rounded-xl bg-black bg-opacity-60">
-                        <h3 className="text-3xl text-white p-2">{playlist.title}</h3>
+                    {/* Playlist */}
+                    <div className="space-y-3 pb-40">
+                        {playlist?.tracks?.data.slice(0, 10).map((play, i) => {
+                            return (
+                                <div key={play.id} onClick={() => { setTrack(i); setPlaying(false) }} className={`flex gap-10 border-b border-blue-900 cursor-pointer hover:bg-blue-900 rounded-lg items-center p-4 ${i === track && 'bg-blue-900'}`}>
+                                    <img className="h-10 w-10 rounded-full" src={play.album.cover_big} alt="" />
+                                    <div className="flex flex-1 items-center justify-between">
+                                        <p className="text-blue-300 w-1/3">{play.title}</p>
+                                        <p className="text-blue-300 w-1/3 text-center">{secondToMinuteAndSeconds(play.duration)}</p>
+                                        <p className="text-blue-300 w-1/3 text-right">{play.artist.name}</p>
+                                    </div>
+                                </div>
+                            )
+                        })}
                     </div>
                 </div>
 
-                {/* Playlist */}
-                <div className="space-y-3 pb-40">
+                {/* Aside */}
+                <div className="flex-grow p-4 h-screen flex flex-col justify-between overflow-scroll scrollbar-hide">
+                    <div className="">
+                        {/* Artists */}
+                        {/* <div className="flex w-full flex-col gap-2">
+                    {artist.slice(0, 5).map((art) => (
+                        <div key={art?.id} className="flex w-[300px] gap-2 text-white">
+                            <img className="h-10 w-10 rounded-lg object-cover" src={art?.user?.header_image_url} alt="" />
+                            <p>{art?.user?.name}</p>
+                        </div>
+                    ))}
+                </div> */}
+                    </div>
+                </div>
+
+                {/* Player */}
+                <div className="bg-blue-900 bg-opacity-90 p-5 fixed bottom-0 left-1/2 -translate-x-1/2 container text-white">
+                    <div>
+                        <p>{secondToMinuteAndSeconds(audioRef?.current?.duration)}</p>
+                    </div>
                     {playlist?.tracks?.data.slice(0, 10).map((play, i) => {
-                        return (
-                            <div key={play.id} onClick={() => { setTrack(i); setPlaying(false) }} className={`flex gap-10 border-b border-blue-900 cursor-pointer hover:bg-blue-900 rounded-lg items-center p-4 ${i === track && 'bg-blue-900'}`}>
-                                <img className="h-10 w-10 rounded-full" src={play.album.cover_big} alt="" />
-                                <div className="flex flex-1 items-center justify-between">
-                                    <p className="text-blue-300 w-1/3">{play.title}</p>
-                                    <p className="text-blue-300 w-1/3 text-center">{secondToMinuteAndSeconds(play.duration)}</p>
-                                    <p className="text-blue-300 w-1/3 text-right">{play.artist.name}</p>
+                        if (i === track) {
+                            return (
+                                <div key={play.id} className="w-full flex justify-between items-center px-10">
+                                    <Image
+                                        height={20}
+                                        width={20}
+                                        className="h-14 w-14 rounded-full"
+                                        src={play.album.cover_big}
+                                        alt={play.artist.name} />
+                                    <p>{play.title}</p>
+                                    <p className="text-white">{play.artist.name}</p>
+                                    <audio src={play.preview} ref={audioRef} />
+                                    <div className="w-8 h-8 bg-blue-900 flex justify-center items-center cursor-pointer rounded-full" onClick={togglePlay}>{playing ? <BsPauseFill className="h-6 w-6" /> : <BsPlayFill className="h-6 w-6" />}</div>
+
                                 </div>
-                            </div>
-                        )
+                            )
+                        }
                     })}
                 </div>
-            </div>
-
-            {/* Aside */}
-            <div className="flex-grow p-4 h-screen flex flex-col justify-between overflow-scroll scrollbar-hide">
-                <div className="">
-                    {/* Artists */}
-                    {/* <div className="flex w-full flex-col gap-2">
-                        {artist.slice(0, 5).map((art) => (
-                            <div key={art?.id} className="flex w-[300px] gap-2 text-white">
-                                <img className="h-10 w-10 rounded-lg object-cover" src={art?.user?.header_image_url} alt="" />
-                                <p>{art?.user?.name}</p>
-                            </div>
-                        ))}
-                    </div> */}
-                </div>
-            </div>
-
-            {/* Player */}
-            <div className="bg-blue-900 bg-opacity-90 p-5 fixed bottom-0 left-1/2 -translate-x-1/2 container text-white">
-                <div>
-                    <p>{secondToMinuteAndSeconds(audioRef?.current?.duration)}</p>
-                </div>
-                {playlist?.tracks?.data.slice(0, 10).map((play, i) => {
-                    if (i === track) {
-                        return (
-                            <div key={play.id} className="w-full flex justify-between items-center px-10">
-                                <Image
-                                    height={20}
-                                    width={20}
-                                    className="h-14 w-14 rounded-full"
-                                    src={play.album.cover_big}
-                                    alt={play.artist.name} />
-                                <p>{play.title}</p>
-                                <p className="text-white">{play.artist.name}</p>
-                                <audio src={play.preview} ref={audioRef} />
-                                <div className="w-8 h-8 bg-blue-900 flex justify-center items-center cursor-pointer rounded-full" onClick={togglePlay}>{playing ? <BsPauseFill className="h-6 w-6" /> : <BsPlayFill className="h-6 w-6" />}</div>
-
-                            </div>
-                        )
-                    }
-                })}
-            </div>
+            </>
         </div>
 
     )
